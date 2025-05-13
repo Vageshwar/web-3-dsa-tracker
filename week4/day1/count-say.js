71bcbc58b12872dd064375ef3d8eb999c5ca3c97
vageshwar <vageshwaryadav@gmail.com>
2025-05-13 10:39:55 +0530

    Week 4: day 1 Compare versions and count say (RLE)

--- week4/day1/count-say.js
+++ week4/day1/count-say.js
@@ -30,10 +30,35 @@
 
 // This is the base case.
 
+var helper = function(s){
+    let newS = "";
+    let c = 1;
+    let t = `${s}$`;
+    for(let i = 1; i <t.length; i++){
+        if(t.charAt(i-1) === t.charAt(i)){
+            c++;
+        }else{
+            newS = `${newS}${c}${t.charAt(i-1)}`;
+            c = 1;
+        }
+    }
+    return newS;
+}
+
 /**
  * @param {number} n
  * @return {string}
  */
 var countAndSay = function(n) {
-    
+    if(n===1) return "1";
+    if(n===2) return "11";
+    let s = "11";
+    for(let i = 2; i<n; i++){
+        s = helper(s);
+        console.log(i,s);
+    }
+    return s;
 };
+
+
+console.log(countAndSay(4));
