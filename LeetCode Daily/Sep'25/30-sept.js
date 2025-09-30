f3eece5e0df9abcca9126ea297e47b6f3c4a8c0f
vageshwar <vageshwaryadav@gmail.com>
2025-09-30 07:53:47 +0530

    LC POTD: 30th Sept '25 Medium Triangular Sum

---
+++ LeetCode Daily/Sep'25/30-sept.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var triangularSum = function(nums) {
+    let current = nums;
+    while (current.length > 1) {
+        const newNums = [];
+        for (let i = 0; i < current.length - 1; i++) {
+            newNums.push((current[i] + current[i + 1]) % 10);
+        }
+        current = newNums;
+    }
+    return current[0];
+};
